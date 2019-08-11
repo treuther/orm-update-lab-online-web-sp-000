2dd9857a353da9f964cd0949a151520f14348cfe
Tracy Reuther <tracyreuther@gmail.com>
2019-08-11 23:00:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(id=nil, name, grade)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
