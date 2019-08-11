3ff5266fb32f77dd9101392897201bd7f6827033
Tracy Reuther <tracyreuther@gmail.com>
2019-08-11 22:22:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,17 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  
 
 
 end
