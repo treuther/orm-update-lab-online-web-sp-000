ffe91a05003f72f11fff34d7c0d69555616c119b
Tracy Reuther <tracyreuther@gmail.com>
2019-08-11 22:26:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,16 @@ class Student
     @id = id
   end
   
-  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
 
 
 end
