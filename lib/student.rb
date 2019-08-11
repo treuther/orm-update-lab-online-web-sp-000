d77601847e987b922b47369f372704055f971976
Tracy Reuther <tracyreuther@gmail.com>
2019-08-11 22:38:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
         VALUES (?, ?)
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute(SELECT last_insert_rowid)
+    @id = DB[:conn].execute(SELECT last_insert_rowid() FROM)
   end
 
 
