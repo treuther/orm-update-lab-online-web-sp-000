b2ae9c89e30328db36145568a66f1578b2334662
Tracy Reuther <tracyreuther@gmail.com>
2019-08-11 23:07:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
